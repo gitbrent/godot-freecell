0823972e0b06a44c1d38f710ded1a5195370a6dc
GitBrent <gitbrent@users.noreply.github.com>
2024-02-19 20:36:43 -0600

    replaced old start screen

--- scripts/main_node.gd
+++ scripts/main_node.gd
@@ -2,10 +2,12 @@ extends Node
 
 @onready var start_scene:Node2D = $StartScene
 @onready var game_scene:Node2D = $GameScene
+@onready var start_scene_2:Node2D = $StartScene2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_scene.connect("start_button_pressed", on_start_button_pressed)
+	start_scene_2.connect("start_button_pressed", on_start_button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,5 +15,6 @@ func _process(delta):
 
 func on_start_button_pressed():
 	start_scene.visible = false
+	start_scene_2.visible = false
 	game_scene.visible = true
 	#pass
