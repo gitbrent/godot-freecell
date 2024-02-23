c76fbdf7a13583ba80bd9c034f5dbd1c58682278
GitBrent <gitbrent@users.noreply.github.com>
2024-02-22 23:59:18 -0600

    added global enums

---
+++ scripts/enums.gd
@@ -0,0 +1,24 @@
+extends Node
+
+enum Suit {
+	HEARTS,
+	DIAMONDS, 
+	CLUBS, 
+	SPADES 
+}
+
+enum Rank { 
+	ACE = 1,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING
+}
