2e65ffe03ac82411078b6a9a587df82c4209f93b
GitBrent <gitbrent@users.noreply.github.com>
2024-02-24 14:51:38 -0600

    card drag working

--- scripts/card.gd
+++ scripts/card.gd
@@ -9,10 +9,29 @@ class_name Card
 var suit: Enums.Suit
 var rank: Enums.Rank
 var location: Vector2  # Current pile/cell position
+var dragging = false
+var drag_offset = Vector2()
 
-func initialize(suit: Enums.Suit, rank: Enums.Rank):
-	self.suit = suit
-	self.rank = rank
+func _on_area_2d_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:  # Start drag
+			print("[card.gd]: Card clicked! " + rank_to_string(rank) +"-"+ suit_to_string(suit))
+			z_index = get_parent().get_child_count()  # bring the card to front on drag start
+			dragging = true
+			drag_offset = global_position - get_global_mouse_position()
+		else:  # Release drag
+			dragging = false
+	elif event is InputEventMouseMotion and dragging:
+		global_position = get_global_mouse_position() + drag_offset
+
+# Depending on your game's rules, you may want to add constraints to dragging 
+# (e.g., only allowing certain cards to be moved or snapping cards back to their original position 
+# if not dropped on a valid target). 
+# For this, you can expand the logic within the _input_event method or introduce new methods to handle these rules.
+
+func initialize(suitIn: Enums.Suit, rankIn: Enums.Rank):
+	self.suit = suitIn
+	self.rank = rankIn
 	self.location = Vector2.ZERO
 	load_texture()
 
@@ -26,20 +45,20 @@ func load_texture():
 	#print('loaded texture: ' + texture_path)
 
 # Helper functions for converting enum values to strings
-func suit_to_string(suit: Enums.Suit) -> String:
-	if suit == Enums.Suit.HEARTS:
+func suit_to_string(suitIn: Enums.Suit) -> String:
+	if suitIn == Enums.Suit.HEARTS:
 		return "hearts"
-	elif suit == Enums.Suit.DIAMONDS:
+	elif suitIn == Enums.Suit.DIAMONDS:
 		return "diamonds"
-	elif suit == Enums.Suit.CLUBS:
+	elif suitIn == Enums.Suit.CLUBS:
 		return "clubs"
-	elif suit == Enums.Suit.SPADES:
+	elif suitIn == Enums.Suit.SPADES:
 		return "spades"
 	else:
 		return "unknown"
 
-func rank_to_string(rank: Enums.Rank) -> String:
-	match rank:
+func rank_to_string(rankIn: Enums.Rank) -> String:
+	match rankIn:
 		Enums.Rank.TWO: return "02"
 		Enums.Rank.THREE: return "03"
 		Enums.Rank.FOUR: return "04"
--- scripts/main_node.gd
+++ scripts/main_node.gd
@@ -7,10 +7,6 @@ extends Node
 func _ready():
 	start_scene.connect("start_button_pressed", on_start_button_pressed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func on_start_button_pressed():
 	start_scene.visible = false
 	game_scene.visible = true
