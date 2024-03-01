c0446e28609a574fcb77a01172bf37c1080f707a
GitBrent <gitbrent@users.noreply.github.com>
2024-02-29 19:08:32 -0600

    code clean up

--- scripts/free_cell.gd
+++ scripts/free_cell.gd
@@ -17,6 +17,11 @@ var card_in_cell : Card = null
 var vertical_offset : int = 1
 var horizontal_offset : int = 1
 
+func _ready():
+	# Create a theme for StyleBox
+	if $Panel.theme == null:
+		$Panel.theme = Theme.new()
+
 func add_card(card: Card):
 	if card is Card:
 		add_child(card) # Add the card to this node
@@ -37,27 +42,15 @@ func remove_all():
 		remove_card(card_in_cell)
 
 func _on_area_2d_area_entered(area):
+	# Emit signal if a card can go here
 	if not card_in_cell:
 		emit_signal("card_hover_free_start", self)
-	
-	# WIP:
-	#print($Panel.theme)
-	# Ensure the Panel has a Theme assigned; if not, create and assign one dynamically
-	if $Panel.theme == null:
-		$Panel.theme = Theme.new()
-
-	#$Panel.add_theme_stylebox_override("hover", free_cell_hover_style)
+		
+	# Apply hover style
 	$Panel.theme.set_stylebox("panel", "Panel", free_cell_hover_style)
-
-	#$Panel.remove_theme_stylebox_override("hover")
-	#print($Panel.theme)
-	#$Panel.stylebox = free_cell_hover_style
-	#print($ColorRect.modulate)
-	#$ColorRect.modulate = Color(1, 0, 0, 1)  # Change color to red for example
-	
 
 func _on_area_2d_area_exited(_area):
 	emit_signal("card_hover_free_ended", self)
 	
-	# WIP:
+	# Reset style
 	$Panel.theme.set_stylebox("panel", "Panel", free_cell_normal_style)
