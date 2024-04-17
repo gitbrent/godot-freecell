7bdf15b9049ef83df60b96faba61c3cdc0c3ce14
GitBrent <gitbrent@users.noreply.github.com>
2024-04-17 18:12:20 -0500

    added card return sound

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -7,8 +7,9 @@ extends Node2D
 @onready var infobox_timer:Label = $InfoRect/HBoxContainer/HBoxContElapsed/Value
 @onready var infobox_score:Label = $InfoRect/HBoxContainer/HBoxContScore/Value
 @onready var main_menu:MainMenu = $MainMenu
-@onready var audio_stream_shuffle:AudioStreamPlayer = $AudioStreamShuffle
-@onready var audio_stream_card_flip:AudioStreamPlayer = $AudioStreamCardFlip
+@onready var audio_shuffle:AudioStreamPlayer = $AudioShuffle
+@onready var audio_card_flip:AudioStreamPlayer = $AudioCardFlip
+@onready var audio_card_nope:AudioStreamPlayer = $AudioCardNope
 
 # VARIABLES
 var drag_offset : Vector2 = Vector2()
@@ -168,18 +169,18 @@ func _on_move_card_seq_tween_completed(src_card, tgt_card, free_cell, fnda_cell,
 		tableau_piles[new_pile_index].add_card(src_card)
 		game_prop_score += 10
 		src_card.show_points(10)
-		audio_stream_card_flip.play()
+		audio_card_flip.play()
 	elif tabl_pile:
 		tabl_pile.add_card(src_card)
 		game_prop_score += 10
 		src_card.show_points(10)
-		audio_stream_card_flip.play()
+		audio_card_flip.play()
 	elif free_cell and free_cells.size() > 0:
 		if dragging_cards.size() == 1:
 			free_cell.add_card(src_card)
 			game_prop_score += 10
 			src_card.show_points(10)
-			audio_stream_card_flip.play()
+			audio_card_flip.play()
 			#break  # Since only one card can be moved to a free cell, break after moving
 		else:
 			print("ERROR: Cannot move more than one card to a Free Cell!")
@@ -190,7 +191,7 @@ func _on_move_card_seq_tween_completed(src_card, tgt_card, free_cell, fnda_cell,
 		fnda_cell.add_card(src_card)
 		game_prop_score += 100
 		src_card.show_points(100)
-		audio_stream_card_flip.play()
+		audio_card_flip.play()
 		#break
 	
 	# If moving the last card in the sequence, reset the dragging cards array and other properties
@@ -303,7 +304,8 @@ func _on_card_drag_ended(card):
 					var tween = get_tree().create_tween()
 					tween.tween_property(card_in_sequence, "global_position", card_in_sequence.original_position, 0.5)
 					tween.tween_callback(reset_card_z_indices)
-#
+					audio_card_nope.play()
+		
 		# Resets
 		card_dragged = null # Ensure to reset this regardless of condition to prevent stuck states
 		hovered_fnda_cell = null
@@ -469,7 +471,7 @@ func deal_cards():
 	var deck = []
 	
 	# STEP 1: audio
-	audio_stream_shuffle.play()
+	audio_shuffle.play()
 
 	# STEP 1: clear all cards
 	clear_deck()
