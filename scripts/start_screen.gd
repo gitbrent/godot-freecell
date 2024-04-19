6263023c50a20db3d873295263395d49e8ab81ff
GitBrent <gitbrent@users.noreply.github.com>
2024-04-18 21:00:28 -0500

    added rotating cards

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -3,6 +3,10 @@ extends Node2D
 signal start_button_pressed()
 
 @onready var audio_btn_click:AudioStreamPlayer = $AudioBtnClick
+@onready var animation_player:AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	animation_player.play("spin_card")
 
 func _on_start_button_pressed():
 	audio_btn_click.play()
