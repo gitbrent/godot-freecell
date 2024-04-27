d60c7ef4d87d542aa1df8474030f8813da8c8fbe
GitBrent <gitbrent@users.noreply.github.com>
2024-04-26 20:53:49 -0500

    updated highlight anim sprrites

--- scripts/card.gd
+++ scripts/card.gd
@@ -13,6 +13,7 @@ signal card_hover_ended(card_dragged, target_card)
 @onready var animation_player : AnimationPlayer = $LabelAnimationPlayer
 @onready var border_anim_rainbow : AnimatedSprite2D = $BorderAnimRainbow
 @onready var border_anim_green_8 : AnimatedSprite2D = $BorderAnimGreen8
+@onready var border_anim_grn_new : AnimatedSprite2D = $BorderAnimGrnNew
 
 # Card properties
 var suit : Enums.Suit
@@ -71,12 +72,14 @@ func _on_area_2d_area_exited(area):
 func style_hovered_on():
 	panel_hover.visible = true
 	#border_anim_rainbow.visible = true
-	border_anim_green_8.visible = true
+	#border_anim_green_8.visible = true
+	border_anim_grn_new.visible = true
 
 func style_hovered_off():
 	panel_hover.visible = false
 	#border_anim_rainbow.visible = false
-	border_anim_green_8.visible = false
+	#border_anim_green_8.visible = false
+	border_anim_grn_new.visible = false
 
 # Call this method to display and animate points
 func show_points(points: int):
